a72d248fd015e6fbcb0af057a9a7f3ac1d23357b
axayagrawal <41827133+axayagrawal@users.noreply.github.com>
2019-04-02 16:46:51 +0530

    Add files via upload

---
+++ ItunesApp/Itunes/Itunes/SongCVC.swift
@@ -0,0 +1,22 @@
+//
+//  SongCVC.swift
+//  Itunes
+//
+//  Created by Student on 02/04/19.
+//  Copyright © 2019 axay. All rights reserved.
+//
+
+import UIKit
+
+class SongCVC: UICollectionViewCell {
+
+    @IBOutlet weak var songImg: UIImageView!
+    @IBOutlet weak var songNamelbl: UILabel!
+    @IBOutlet weak var songArtistlbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
